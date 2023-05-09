f00416db7e466be42462a199bf64c6d5724338e7
Fernanda5510 <130413112+Fernanda5510@users.noreply.github.com>
2023-05-08 22:10:39 -0300

    Update Media_das_salas(Array).cpp

--- Media_das_salas(Array).cpp
+++ Media_das_salas(Array).cpp
@@ -3,17 +3,12 @@ using namespace std;
 int main() {
 //criação da array e da variavel media
 int salas[6], media = 0;
-//definição da quantidade de alunos;
-salas[0] = 10;
-salas[1] = 50;
-salas[2] = 30;
-salas[3] = 40;
-salas[4] = 60;
-salas[5] = 20;
 
-//soma do numero de alunos para a media adicionado em contagem para facilitar o processo
+//O usuario ira digitar o numero de alunos que ha em cada sala e logo em seguida este numero sera adicionado a media
 for(int i = 0; i < 6; i++) {
-media = media+salas[i];
+    cout << "Digite o numero de alunos da sala" << i+1 << endl;
+    cin >> salas[i];
+    media = media+salas[i];
 }
 //divisao para a finalizacao do calculo da media
 media =media/6;
@@ -23,7 +18,7 @@ cout <<"A media de alunos é " << media << endl;
 for(int i=0; i<6; i++){
     
     if (salas[i]>= media){
-        cout <<"A sala "<<i+1 <<" tem mais alunos que a media" << endl;
+        cout <<"A sala "<<i+1 <<" tem um numero de alunos maior ou igual a media" << endl;
     }
 }
 
