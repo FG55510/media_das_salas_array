1c253f61a0391769a6de43d1584b6c157d9d17a1
Fernanda5510 <130413112+Fernanda5510@users.noreply.github.com>
2023-05-08 22:12:16 -0300

    Update Media_das_salas(Array).cpp

--- Media_das_salas(Array).cpp
+++ Media_das_salas(Array).cpp
@@ -10,13 +10,13 @@ for(int i = 0; i < 6; i++) {
     cin >> salas[i];
     media = media+salas[i];
 }
+    
 //divisao para a finalizacao do calculo da media
 media =media/6;
 cout <<"A media de alunos é " << media << endl;
 
 //Para cada sala se ela conter um numero maior ou igual o da media, o programa informara ao usuario
 for(int i=0; i<6; i++){
-    
     if (salas[i]>= media){
         cout <<"A sala "<<i+1 <<" tem um numero de alunos maior ou igual a media" << endl;
     }
